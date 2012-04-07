2c3fef1ee4e959bed8ae70cb835732524fd4164e
Joel Costigliola <joel.costigliola@gmail.com>
2012-04-07 22:07:22 +0200

    Complete conditions examples

--- src/main/java/org/fest/assertions/examples/advanced/UsingConditionExamples.java
+++ src/main/java/org/fest/assertions/examples/advanced/UsingConditionExamples.java
@@ -25,59 +25,60 @@ public class UsingConditionExamples extends AbstractAssertionsExamples {
     assertThat("Yoda").has(jediPower);
     assertThat("Solo").doesNotHave(jediPower);
   }
-  
+
   @Test
   public void condition_example_on_multiple_elements() {
     // are & areNot
     assertThat(set("Luke", "Yoda")).are(jedi);
     assertThat(set("Leia", "Solo")).areNot(jedi);
-    
+
     // have & doNotHave
     assertThat(set("Luke", "Yoda")).have(jediPower);
     assertThat(set("Leia", "Solo")).doNotHave(jediPower);
-    
+
     // areAtLeast & areNotAtLeast
     assertThat(set("Luke", "Yoda", "Leia")).areAtLeast(2, jedi);
     assertThat(set("Luke", "Yoda", "Obiwan")).areAtLeast(2, jedi);
     assertThat(set("Luke", "Yoda", "Leia")).areNotAtLeast(1, jedi);
     assertThat(set("Luke", "Solo", "Leia")).areNotAtLeast(1, jedi);
-    
+
     // haveAtLeast & doNotHaveAtLeast
     assertThat(set("Luke", "Yoda", "Leia")).haveAtLeast(2, jediPower);
     assertThat(set("Luke", "Yoda", "Obiwan")).haveAtLeast(2, jediPower);
-    assertThat(set("Luke", "Yoda", "Leia")).doNotHaveAtLeast(1, jediPower);   
+    assertThat(set("Luke", "Yoda", "Leia")).doNotHaveAtLeast(1, jediPower);
     assertThat(set("Luke", "Solo", "Leia")).doNotHaveAtLeast(1, jediPower);
-    
+
     // areAtMost & areNotAtMost
     assertThat(set("Luke", "Yoda", "Leia")).areAtMost(2, jedi);
     assertThat(set("Luke", "Solo", "Leia")).areAtMost(2, jedi);
     assertThat(set("Luke", "Yoda", "Leia")).areNotAtMost(1, jedi);
-    assertThat(set("Luke", "Yoda", "Obiwan")).areNotAtMost(1, jedi);   
-    
+    assertThat(set("Luke", "Yoda", "Obiwan")).areNotAtMost(1, jedi);
+
     // haveAtMost & doNotHaveAtMost
     assertThat(set("Luke", "Yoda", "Leia")).haveAtMost(2, jediPower);
     assertThat(set("Luke", "Solo", "Leia")).haveAtMost(2, jediPower);
     assertThat(set("Luke", "Yoda", "Leia")).doNotHaveAtMost(1, jediPower);
-    assertThat(set("Luke", "Yoda", "Obiwan")).doNotHaveAtMost(1, jediPower);   
-    
+    assertThat(set("Luke", "Yoda", "Obiwan")).doNotHaveAtMost(1, jediPower);
+
     // areExactly & areNotExactly
     assertThat(set("Luke", "Yoda", "Leia")).areExactly(2, jedi);
     assertThat(set("Luke", "Yoda", "Leia")).areNotExactly(1, jedi);
-    
+
     // haveExactly & haveNotExactly
     assertThat(set("Luke", "Yoda", "Leia")).haveExactly(2, jediPower);
-    assertThat(set("Luke", "Yoda", "Leia")).doNotHaveExactly(1, jediPower);    
+    assertThat(set("Luke", "Yoda", "Leia")).doNotHaveExactly(1, jediPower);
   }
-  
+
   @SuppressWarnings("unchecked")
   @Test
   public void has_not_condition_example() {
     assertThat("Yoda").has(jediPower);
     assertThat("Yoda").has(allOf(jediPower, not(sithPower)));
     assertThat("Solo").has(not(jediPower));
+    assertThat("Solo").doesNotHave(jediPower);
     assertThat("Solo").is(allOf(not(jedi), not(sith)));
-    }
-  
+  }
+
   private final Condition<String> jedi = new Condition<String>("jedi") {
     private final Set<String> jedis = set("Luke", "Yoda", "Obiwan");
 
@@ -89,29 +90,29 @@ public class UsingConditionExamples extends AbstractAssertionsExamples {
 
   private final Condition<String> jediPower = new Condition<String>("jedi power") {
     private final Set<String> jedis = set("Luke", "Yoda", "Obiwan");
-    
+
     @Override
     public boolean matches(String value) {
       return jedis.contains(value);
     };
   };
-  
+
   private final Condition<String> sith = new Condition<String>("sith") {
     private final Set<String> siths = set("Sidious", "Vader", "Plagueis");
-    
+
     @Override
     public boolean matches(String value) {
       return siths.contains(value);
     };
   };
-  
+
   private final Condition<String> sithPower = new Condition<String>("sith power") {
     private final Set<String> siths = set("Sidious", "Vader", "Plagueis");
-    
+
     @Override
     public boolean matches(String value) {
       return siths.contains(value);
     };
   };
-  
+
 }
