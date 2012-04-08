b538204672a391048e06b382105f4c1c502fc59f
Joel Costigliola <joel.costigliola@gmail.com>
2012-04-08 17:23:53 +0200

    Show how to have a single entry point for map assertions

--- src/main/java/org/fest/assertions/examples/MapAssertionsExamples.java
+++ src/main/java/org/fest/assertions/examples/MapAssertionsExamples.java
@@ -1,7 +1,6 @@
 package org.fest.assertions.examples;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-import static org.fest.assertions.data.MapEntry.entry;
+import static org.fest.assertions.api.Assertions.*;
 import static org.fest.assertions.examples.data.Ring.*;
 
 import org.junit.Test;
@@ -18,7 +17,7 @@ public class MapAssertionsExamples extends AbstractAssertionsExamples {
     // ringBearers is a Map of TolkienCharacter indexed by the Ring they are wearing.
     assertThat(ringBearers).isNotEmpty().hasSize(4);
     
-    // note the usage of MapEntry.entry(key, value) synthetic sugar for better readability. 
+    // note the usage of Assertions.entry(key, value) synthetic sugar for better readability (similar to MapEntry.entry(key, value)). 
     assertThat(ringBearers).contains(entry(oneRing, frodo), entry(nenya, galadriel));
     assertThat(ringBearers).doesNotContain(entry(oneRing, aragorn));
     
