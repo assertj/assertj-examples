7ef8840ef40bce77fa06dfc9483e9719d31f35d3
Joel Costigliola <joel.costigliola@gmail.com>
2014-08-02 19:07:28 +1200

    Add assertion examples for Comparable classes

---
+++ assertions-examples/src/test/java/org/assertj/examples/ComparableAssertionsExamples.java
@@ -0,0 +1,62 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.examples;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Comparable assertions specific examples
+ * 
+ * @author Joel Costigliola
+ */
+public class ComparableAssertionsExamples extends AbstractAssertionsExamples {
+
+  @Test
+  public void comparable_assertions_examples() {
+
+    Rating goodRating = new Rating(8);
+    Rating badRating = new Rating(4);
+    assertThat(goodRating).isGreaterThan(badRating);
+    assertThat(goodRating).isGreaterThanOrEqualTo(badRating);
+    assertThat(badRating).isLessThan(goodRating);
+    assertThat(badRating).isLessThanOrEqualTo(goodRating);
+    try {
+      assertThat(badRating).isGreaterThan(goodRating);
+    } catch (AssertionError e) {
+      logAssertionErrorMessage("isGreaterThan for generic", e);
+    }
+
+  }
+
+  private static class Rating implements Comparable<Rating> {
+
+    private int note;
+
+    public Rating(int note) {
+      this.note = note;
+    }
+
+    @Override
+    public int compareTo(Rating o) {
+      return note - o.note;
+    }
+    
+    @Override
+    public String toString() {
+      return String.valueOf(note);
+    }
+  }
+
+}
