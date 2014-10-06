05d8c91134b66d104c2a8bc190266c2aca1101cc
Joel Costigliola <joel.costigliola@gmail.com>
2014-10-06 20:59:16 +1300

    Commit generated assertions to be able to import project in eclipse without compilation error

---
+++ assertions-examples/src/test/generated-assertions/org/assertj/BddAssertions.java
@@ -0,0 +1,275 @@
+package org.assertj;
+
+/**
+ * Entry point for BDD assertions of different data types. Each method in this class is a static factory for the
+ * type-specific assertion objects.
+ */
+public class BddAssertions {
+
+  /**
+   * Creates a new instance of <code>{@link com.google.common.net.HostAndPortAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static com.google.common.net.HostAndPortAssert then(com.google.common.net.HostAndPort actual) {
+    return new com.google.common.net.HostAndPortAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link com.google.common.net.InetAddressesAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static com.google.common.net.InetAddressesAssert then(com.google.common.net.InetAddresses actual) {
+    return new com.google.common.net.InetAddressesAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link com.google.common.net.InternetDomainNameAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static com.google.common.net.InternetDomainNameAssert then(com.google.common.net.InternetDomainName actual) {
+    return new com.google.common.net.InternetDomainNameAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.AlignmentAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.AlignmentAssert then(org.assertj.examples.data.Alignment actual) {
+    return new org.assertj.examples.data.AlignmentAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.BasketBallPlayerAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.BasketBallPlayerAssert then(org.assertj.examples.data.BasketBallPlayer actual) {
+    return new org.assertj.examples.data.BasketBallPlayerAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.BookAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.BookAssert then(org.assertj.examples.data.Book actual) {
+    return new org.assertj.examples.data.BookAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.BookTitleAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.BookTitleAssert then(org.assertj.examples.data.Book.Title actual) {
+    return new org.assertj.examples.data.BookTitleAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.EmployeeAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.EmployeeAssert then(org.assertj.examples.data.Employee actual) {
+    return new org.assertj.examples.data.EmployeeAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.MagicalAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.MagicalAssert then(org.assertj.examples.data.Magical actual) {
+    return new org.assertj.examples.data.MagicalAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.MansionAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.MansionAssert then(org.assertj.examples.data.Mansion actual) {
+    return new org.assertj.examples.data.MansionAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.NameAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.NameAssert then(org.assertj.examples.data.Name actual) {
+    return new org.assertj.examples.data.NameAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.PersonAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.PersonAssert then(org.assertj.examples.data.Person actual) {
+    return new org.assertj.examples.data.PersonAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.RaceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.RaceAssert then(org.assertj.examples.data.Race actual) {
+    return new org.assertj.examples.data.RaceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.RingAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.RingAssert then(org.assertj.examples.data.Ring actual) {
+    return new org.assertj.examples.data.RingAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.TeamAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.TeamAssert then(org.assertj.examples.data.Team actual) {
+    return new org.assertj.examples.data.TeamAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.TolkienCharacterAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.TolkienCharacterAssert then(org.assertj.examples.data.TolkienCharacter actual) {
+    return new org.assertj.examples.data.TolkienCharacterAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.bug18.Dollar$Assert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.bug18.Dollar$Assert then(org.assertj.examples.data.bug18.Dollar$ actual) {
+    return new org.assertj.examples.data.bug18.Dollar$Assert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.bug26.WithGenericListTypeAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.bug26.WithGenericListTypeAssert then(org.assertj.examples.data.bug26.WithGenericListType actual) {
+    return new org.assertj.examples.data.bug26.WithGenericListTypeAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.bug27.MyIteratorWrapperAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.bug27.MyIteratorWrapperAssert then(org.assertj.examples.data.bug27.MyIteratorWrapper actual) {
+    return new org.assertj.examples.data.bug27.MyIteratorWrapperAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.bug27.MyModelClassAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.bug27.MyModelClassAssert then(org.assertj.examples.data.bug27.MyModelClass actual) {
+    return new org.assertj.examples.data.bug27.MyModelClassAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.bug27.MyModelClassMyBeanAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.bug27.MyModelClassMyBeanAssert then(org.assertj.examples.data.bug27.MyModelClass.MyBean actual) {
+    return new org.assertj.examples.data.bug27.MyModelClassMyBeanAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.movie.MovieAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.movie.MovieAssert then(org.assertj.examples.data.movie.Movie actual) {
+    return new org.assertj.examples.data.movie.MovieAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.movie.TeamAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.movie.TeamAssert then(org.assertj.examples.data.movie.Team actual) {
+    return new org.assertj.examples.data.movie.TeamAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.neo4j.DragonBallGraphAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.neo4j.DragonBallGraphAssert then(org.assertj.examples.data.neo4j.DragonBallGraph actual) {
+    return new org.assertj.examples.data.neo4j.DragonBallGraphAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.service.GameServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.service.GameServiceAssert then(org.assertj.examples.data.service.GameService actual) {
+    return new org.assertj.examples.data.service.GameServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.assertj.examples.data.service.TeamManagerAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  public static org.assertj.examples.data.service.TeamManagerAssert then(org.assertj.examples.data.service.TeamManager actual) {
+    return new org.assertj.examples.data.service.TeamManagerAssert(actual);
+  }
+
+  /**
+   * Creates a new <code>{@link BddAssertions}</code>.
+   */
+  protected BddAssertions() {
+    // empty
+  }
+}
