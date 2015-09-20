22a9c11ba00078ab67756dab688c6e2fdf72a0c9
Joel Costigliola <joel.costigliola@gmail.com>
2015-09-20 18:28:46 +1200

    Add CompletableFuture assertions example

---
+++ assertions-examples/src/test/java/org/assertj/examples/CompletableFutureAssertionsExamples.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.examples;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.Test;
+
+/**
+ * {@link CompletableFuture} assertions example.
+ * 
+ * @author Joel Costigliola
+ */
+public class CompletableFutureAssertionsExamples extends AbstractAssertionsExamples {
+
+  @Test
+  public void completableFuture_assertions_examples() {
+
+    CompletableFuture<String> completedFuture = CompletableFuture.completedFuture("something");
+    assertThat(completedFuture).isCompleted()
+                               .isCompletedWithValue("something")
+                               .isCompletedWithValueMatching(result -> result.startsWith("some"))
+                               .isCompletedWithValueMatching(result -> result.startsWith("some"), "error message")
+                               .isDone();
+
+    CompletableFuture<?> futureExplosion = new CompletableFuture<>();
+    futureExplosion.completeExceptionally(new RuntimeException("boom !"));
+    assertThat(futureExplosion).isCompletedExceptionally()
+                               .isDone();
+
+    // failed = CompletedExceptionally() + isNotCancelled()
+    assertThat(futureExplosion).hasFailed()
+                               .hasFailedWithThrowableThat().isInstanceOf(RuntimeException.class).hasMessage("boom !");
+
+    CompletableFuture<?> cancelledFuture = new CompletableFuture<>();
+    cancelledFuture.cancel(true);
+    assertThat(cancelledFuture).isCancelled()
+                               .isDone()
+                               .hasNotFailed();
+
+    // log some error messages to have a look at them
+    try {
+      assertThat(completedFuture).isCompletedWithValueMatching(result -> result == null, "expected null");
+    } catch (AssertionError e) {
+      logAssertionErrorMessage("CompletableFutureAssert.isCompletedWithValueMatching", e);
+    }
+  }
+
+}
