5699ac3cf6612cf002ec7ee596fd6ad771114ee7
Joel Costigliola <joel.costigliola@gmail.com>
2015-11-15 21:24:47 +1300

    Add map assertions with java.util.Map.Entry

--- assertions-examples/src/test/java/org/assertj/examples/MapAssertionsExamples.java
+++ assertions-examples/src/test/java/org/assertj/examples/MapAssertionsExamples.java
@@ -1,10 +1,13 @@
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
+ *
  * Copyright 2012-2014 the original author or authors.
  */
 package org.assertj.examples;
@@ -18,6 +21,7 @@ import static org.assertj.examples.data.Ring.nenya;
 import static org.assertj.examples.data.Ring.oneRing;
 import static org.assertj.examples.data.Ring.vilya;
 
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -41,11 +45,13 @@ public class MapAssertionsExamples extends AbstractAssertionsExamples {
     // note the usage of Assertions.entry(key, value) synthetic sugar for better readability (similar to
     // MapEntry.entry(key, value)).
     assertThat(ringBearers).contains(entry(oneRing, frodo), entry(nenya, galadriel));
+    // using java util Map.Entry
+    assertThat(ringBearers).contains(javaMapEntry(oneRing, frodo), javaMapEntry(nenya, galadriel));
     // same assertion but different way of expressing it : no entry call needed but no varargs support.
     assertThat(ringBearers).containsEntry(oneRing, frodo).containsEntry(nenya, galadriel);
     // opposite of contains/containsEntry
     assertThat(ringBearers).doesNotContain(entry(oneRing, sauron), entry(nenya, aragorn));
-    assertThat(ringBearers).doesNotContainEntry(oneRing, aragorn);
+    assertThat(ringBearers).doesNotContainEntry(oneRing, aragorn); 
 
     // Assertion on key
     assertThat(ringBearers).containsKey(nenya);
@@ -128,5 +134,9 @@ public class MapAssertionsExamples extends AbstractAssertionsExamples {
     // assertThat(new JSONObject(ringBearers)).as("").containsKey(nenya);
     // Object o = nenya;
     // assertThat(data).as("ssss").containsKey(o);
+  }
+
+  private static <K, V> Map.Entry<K, V> javaMapEntry(K key, V value) {
+    return new SimpleImmutableEntry<K, V>(key, value);
   }
 }
--- assertions-examples/src/test/java/org/assertj/examples/NumberAssertionsExamples.java
+++ assertions-examples/src/test/java/org/assertj/examples/NumberAssertionsExamples.java
@@ -1,10 +1,13 @@
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
+ *
  * Copyright 2012-2014 the original author or authors.
  */
 package org.assertj.examples;
@@ -98,7 +101,7 @@ public class NumberAssertionsExamples extends AbstractAssertionsExamples {
     // With BigDecimal, 8.0 is not equals to 8.00 but it is if you use compareTo()
     assertThat(new BigDecimal("8.0")).isEqualByComparingTo(new BigDecimal("8.00"));
     assertThat(new BigDecimal("8.0")).isEqualByComparingTo("8.00");
-    assertThat(new BigDecimal("8.0")).isNotEqualByComparingTo("8.00");
+    assertThat(new BigDecimal("8.0")).isNotEqualByComparingTo("8.01");
 
     // isGreaterThanOrEqualTo uses compareTo semantics
     assertThat(new BigDecimal("8.0")).isGreaterThanOrEqualTo(new BigDecimal("8.00"));
