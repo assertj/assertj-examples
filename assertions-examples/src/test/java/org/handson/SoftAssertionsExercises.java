10c988d137df0475fc8268a9a941b4901fe57d24
Joel Costigliola <joel.costigliola@orionhealth.com>
2017-11-15 18:03:59 +1300

    Add SoftAssertionsExercises

---
+++ assertions-examples/src/test/java/org/handson/SoftAssertionsExercises.java
@@ -0,0 +1,37 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2016 the original author or authors.
+ */
+package org.handson;
+
+import org.assertj.examples.AbstractAssertionsExamples;
+import org.junit.Test;
+
+public class SoftAssertionsExercises extends AbstractAssertionsExamples {
+
+	// the data used are initialized in AbstractAssertionsExamples.
+
+	@Test
+	public void exercise_1_use_basic_soft_assertions() {
+		// TODO write soft assertions about the fellowshipOfTheRing list (or whatever other stuff you want to test)
+	}
+
+	@Test
+	public void exercise_2_use_JUnitSoftAssertions() {
+		// TODO rewrite the previous with JUnitSoftAssertions
+	}
+
+	@Test
+	public void exercise_3_BDD_style_soft_assertions() {
+		// TODO : give a try to JUnitBDDSoftAssertions.
+	}
+
+}
